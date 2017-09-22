393a35d87a08604ef38ff2121e49557735a187e9
Raghavendra Vinnakota <raghavendra.vinnakota@nokia.com>
2017-09-22 15:14:28 +0530

    first commit

---
+++ src/test/java/com/motive/smp/smpresource/smpresourceservice/SmpresourceserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.motive.smp.smpresource.smpresourceservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmpresourceserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
